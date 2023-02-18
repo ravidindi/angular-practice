a8794f9dbac0e9911ef72447db30b616c3023923
ravidindi <ravidindisaiprasannakumar@gmail.com>
2023-02-19 02:06:26 +0530

    custom-directive

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,23 @@
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    console.log(this.isOpen);
+    console.log(this.elRef.nativeElement);
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
+  }
+
+  constructor(private elRef: ElementRef) {}
+}
